f1ce6d6f2ef72e8beecec9b00028496f7a64df46
Iliyan Gogov <iliangogov@gmail.com>
2017-03-31 22:26:54 +0300

    Calculator demo finished

--- Calculator-Demo/Calculator-Demo/ViewController.swift
+++ Calculator-Demo/Calculator-Demo/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var result: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstNumber.delegate = self
+        secondNumber.delegate = self
+        self.firstNumber.text = "0"
+        self.secondNumber.text = "0"
+        self.result.text = "0"
+        
+        self.firstNumber.keyboardType = .numberPad
+        
+        self.secondNumber.keyboardType = .numberPad
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let invalidCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        return string.rangeOfCharacter(from: invalidCharacters, options: [], range: string.startIndex ..< string.endIndex) == nil
+    }
 
+    @IBAction func calculateOnClick() {
+        let firstOperand  = Int(self.firstNumber.text!)!
+        let secondOperand = Int(self.secondNumber.text!)!
+        
+        self.result.text = String(firstOperand + secondOperand)
+        }
 }
 
