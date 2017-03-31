1018f0878436dae016f48bb2560c58bb56cb888a
Iliyan Gogov <iliangogov@gmail.com>
2017-03-31 11:08:20 +0300

    Calculator demo

---
+++ Calculator-Demo/Calculator-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator-Demo
+//
+//  Created by Iliyan Gogov on 3/31/17.
+//  Copyright © 2017 Iliyan Gogov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
